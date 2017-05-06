feef81083fdc963ebe69e770f4c94e6b32216083
James Allen-Robertson <minyall@gmail.com>
2017-05-07 00:14:57 +0100

    redirected import to use modified spider package.

--- protocol/http_scanner.go
+++ protocol/http_scanner.go
@@ -5,7 +5,7 @@ import (
 	"github.com/s-rah/onionscan/config"
 	"github.com/s-rah/onionscan/report"
 
-	"github.com/s-rah/onionscan/spider"
+	"github.com/Minyall/onionscan/spider"
 	"github.com/s-rah/onionscan/utils"
 	"net/http"
 )
